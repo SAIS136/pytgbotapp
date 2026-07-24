d91fce1061aaa2bf5d5dd90d7d633533ec5ecf16
saizawlinphyo <saizawlinphyo@gmail.com>
2026-07-24 21:27:35 +0630

    Second step

--- databaseconfig.py
+++ databaseconfig.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+import certifi
 
 
 load_dotenv()
@@ -9,7 +10,18 @@ load_dotenv()
 mongo_uri = os.getenv("MONGO_URI") # mongodb://localhost:27017/
 
 # Connect to MongoDB server
-client = MongoClient(mongo_uri)
+client = MongoClient(
+    mongo_uri,
+    tls=True,
+    tlsCAFile=certifi.where(),
+    serverSelectionTimeoutMS=10000
+)
+
+try:
+    client.admin.command("ping")
+    print("MongoDB Atlas Connected Successfully")
+except Exception as e:
+    print("MongoDB Error:", e)
 
 # Select database
 db = client["mydatabase"]
